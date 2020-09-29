d85351180eb6b39e18fc58702266f8f1feed9f24
Henry Groover <henry.groover@gmail.com>
2020-09-29 04:36:26 -0500

    First attempt at rotary encoder filament sensor. Requires one 3/8 x 3/4" compression spring and 7 US #6 screws (to be cloned for metric version).

---
+++ 3dprint-cnc/rotary-filament-sensor/rotary-encoder-test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# From https://www.raspberrypi.org/forums/viewtopic.php?t=198815 (3rd snippet)
+# May require sudo apt-get install python-gpiozero
+# With the knob stem facing you and three pins on the top, A is rightmost, GND is center,
+# and B is leftmost.
+
+from gpiozero import Button
+import Queue
+
+eventq = Queue.Queue()
+
+pin_a = Button(2)                      # Rotary encoder pin A connected to GPIO2
+pin_b = Button(3)                      # Rotary encoder pin B connected to GPIO3
+
+def pin_a_rising():                    # Pin A event handler
+    if pin_b.is_pressed: eventq.put(-1)# pin A rising while A is active is a clockwise turn
+
+def pin_b_rising():                    # Pin B event handler
+    if pin_a.is_pressed: eventq.put(1) # pin B rising while A is active is a clockwise turn
+
+pin_a.when_pressed = pin_a_rising      # Register the event handler for pin A
+pin_b.when_pressed = pin_b_rising      # Register the event handler for pin B
+
+while True:
+    message = eventq.get()
+    print(message)
